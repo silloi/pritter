605702c87f9a3cf412bfe161a9b27a87da37be94
silloi <silloi1993@gmail.com>
2023-11-28 22:42:35 +0900

    create form for post

--- app.py
+++ app.py
@@ -1,53 +1,61 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 import requests
 import subprocess
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    paragraph = "<p>Hello, World!</p>"
-    return paragraph
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        userid = request.form.get('userid')
+        message = request.form.get('message')
 
+        try:
+            # Get the URL from the query parameter
+            message = request.form.get('message')
+            if not message:
+                return 'No message provided', 400
 
-@app.route('/print_online_picture')
-def print_online_picture():
-    try:
-        # Get the URL from the query parameter
-        message = request.args.get(
-            'message')
-        if not message:
-            return 'No message provided', 400
+            username = request.form.get('username')
+            userid = request.form.get('userid')
 
-        # Fetch the image
-        response = requests.get(
-            f"https://vercel-og-pritter.vercel.app/api/static?message={message}")
-        if response.status_code != 200:
-            return 'Failed to fetch image', 500
+            url = f"https://vercel-og-pritter.vercel.app/api/static?message={message}"
+            if username:
+                url += f"&username={username}"
+            if userid:
+                url += f"&userid={userid}"
 
-        # Save the image temporarily
-        temp_image_path = 'temp_image.png'
-        with open(temp_image_path, 'wb') as file:
-            file.write(response.content)
+            # Fetch the image
+            response = requests.get(url)
+            if response.status_code != 200:
+                return 'Failed to fetch image', 500
 
-        # Define the command and arguments for subprocess
-        command = 'tools/phomemo-filter.py'
-        args = [temp_image_path]
+            # Save the image temporarily
+            temp_image_path = 'temp_image.png'
+            with open(temp_image_path, 'wb') as file:
+                file.write(response.content)
 
-        # Execute the command and redirect output to /dev/rfcomm0
-        with open('/dev/rfcomm0', 'wb') as device:
-            result = subprocess.run([command] + args, stdout=device)
+            # Define the command and arguments for subprocess
+            command = 'tools/phomemo-filter.py'
+            args = [temp_image_path]
 
-        # Check if the command was successful
-        if result.returncode == 0:
-            return 'Printed successfully'
-        else:
-            return 'Error in printing', 500
+            # Execute the command and redirect output to /dev/rfcomm0
+            with open('/dev/rfcomm0', 'wb') as device:
+                result = subprocess.run([command] + args, stdout=device)
 
-    except Exception as e:
-        # Handle any exceptions
-        return str(e), 500
+            # Check if the command was successful
+            if result.returncode == 0:
+                return 'Printed successfully'
+            else:
+                return 'Error in printing', 500
+
+        except Exception as e:
+            # Handle any exceptions
+            return str(e), 500
+
+    return render_template('form.html')
 
 
 if __name__ == '__main__':
